2cb38ba86ce2f32c4a58815358c31aee907071c7
Dmitry Boris <boris.dmitriy00@mail.ru>
2024-01-06 16:40:45 +0300

    refactor(extended-1.2): update CSS and JS according to Media alternatives rule

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt="remove-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
